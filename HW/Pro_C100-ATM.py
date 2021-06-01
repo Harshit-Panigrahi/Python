3c04a9a56a0d890ca39872182461f73c23f03f85
Wizard Bot 86 <61592605+Wizard-Bot-86@users.noreply.github.com>
2021-06-01 12:28:28 +0530

    Update Pro_C100-ATM.py

--- HW/Pro_C100-ATM.py
+++ HW/Pro_C100-ATM.py
@@ -21,11 +21,11 @@ class ATM():
   def switch(self):
     if self.symbol == '₹':
       self.symbol = '$'
-      self.balance /= 74
+      self.balance = round(self.balance / 74, 2)
       print('Switched from INR(₹) to USD($).')
     elif self.symbol == '$':
       self.symbol = '₹'
-      self.balance *= 74
+      self.balance = round(self.balance * 74, 2)
       print('Switched from USD($) to INR(₹).')
 
   def check(self):
