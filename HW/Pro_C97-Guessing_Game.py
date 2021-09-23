4deaa62474c39355433b68e6aabd36c75afbd812
Wizard Bot 86 <harshit.whitehat@gmail.com>
2021-09-23 13:48:52 +0530

    Update Pro_C97-Guessing_Game.py

--- HW/Pro_C97-Guessing_Game.py
+++ HW/Pro_C97-Guessing_Game.py
@@ -1,3 +1,7 @@
+# NO, there isn't any sneaky virus in this code.
+# All you have to do is download the file and run it in your command line interpreter (like Powershell)
+# If you don't know what that means, it's recommended that you don't download tis file.
+
 import time, random
 
 again = True
