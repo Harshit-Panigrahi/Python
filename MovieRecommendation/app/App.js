8a32b56107f83096eb798d9310675d2dae722219
Wizard Bot 86 <harshit.whitehat@gmail.com>
2021-09-28 18:02:12 +0530

    Add files via upload

---
+++ MovieRecommendation/app/App.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import HomeScreen from "./screens/Home";
+
+export default function App() {
+  return (
+    <HomeScreen />
+  );
+}
